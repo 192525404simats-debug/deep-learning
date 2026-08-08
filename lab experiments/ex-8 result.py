4e4a1f0704501d9afefcd1c1910f260efba347e7
192525404simats-debug <192525404.simats@saveetha.com>
2026-08-08 11:46:20 +0530

    Add files via upload

---
+++ lab experiments/ex-8 result.py
@@ -0,0 +1,16 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=============================================================== RESTART: C:/Users/SRI LAKSHMI MENTHULA/Downloads/ex-6.py ===============================================================
+Traceback (most recent call last):
+  File "C:/Users/SRI LAKSHMI MENTHULA/Downloads/ex-6.py", line 5, in <module>
+    import seaborn as sns
+ModuleNotFoundError: No module named 'seaborn'
+>>> 
+=============================================================== RESTART: C:/Users/SRI LAKSHMI MENTHULA/Downloads/ex-6.py ===============================================================
+accuracy: 0.7037037037037037
+
+=============================================================== RESTART: C:/Users/SRI LAKSHMI MENTHULA/Downloads/ex-7.py ===============================================================
+
+=============================================================== RESTART: C:/Users/SRI LAKSHMI MENTHULA/Downloads/ex-8.py ===============================================================
+accuracy: 0.9777777777777777
